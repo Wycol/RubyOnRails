cb619a138f5e4e014b9a1e1d826a48aed01c8939
Wycol <raulangulomar@gmail.com>
2019-03-28 17:51:29 +0100

    ultimo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,23 +1,52 @@
 class TodosController < ApplicationController
+    def index
+      @todos = Todo.all
+    end
+
     def new
-    @todo = Todo.new
+      @todo = Todo.new
     end
 
     def create
         @todo = Todo.new(todo_params)
         if @todo.save
-            flash[:notice]= "Se ha creado el objeto todo"
-        redirect_to todo_path(@todo)
+          flash[:notice]= "Se ha creado el objeto todo"
+          redirect_to todo_path(@todo)
         else 
-            render 'new'
+          render 'new'
         end
     end
+
     def show
         @todo = Todo.find(params[:id])
     end
+
+    def edit
+        @todo = Todo.find(params[:id])
+    end
+
+    def update
+      if @todo.update(todo_params)
+        flash[:success] = 'Se ha guardado el todo'
+        redirect_to todos_path
+      else
+        flash[:danger] = 'No se ha podido modificar el todo'
+        render :edit
+      end
+    end
+
+    def destroy
+      if @todo.destroy
+        flash[:success] = 'Todo eliminado'
+      else
+        flash[:danger] = 'No se ha podido eliminar'
+      end
+      redirect_back(fallback_location: todos_path)
+    end
+
     private
 
     def todo_params
-    params.require(:todo).permit(:name, :description)
+      params.require(:todo).permit(:name, :description)
     end
 end
